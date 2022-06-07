14afac760450a086184ea856e862dd27176d3c2f
Your Name <sshh7050@naver.com>
2022-06-07 16:16:43 +0900

    2022/06/07 수업예재

---
+++ Ch04/3_MethodOverload.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 : 2022/06/07
+ * 이름 : 이서희
+ * 내용 : 메서드 오버로딩 실습하기 교재 p275
+ * 
+ * 메서드 오버로딩
+ * - 같은 이름의 메서드를 매개변수로 구분한 메서드
+ * -메서드 반환타입은 오버로딩에 영향을 미치지 않는다.
+ */
+namespace Ch04
+{
+    internal class MethodOverload
+    {
+        static void Main1(string[] args)
+        {
+            int r1 = Plus(1, 2);
+            int r2 = Plus(1, 2, 3);
+            double r3 = Plus(1.01, 2.3);
+            string r4 = Plus("Hello", "World");
+
+            Console.WriteLine("r1 : "+r1  );
+            Console.WriteLine("r2 : "+r2  );
+            Console.WriteLine("r3 : "+r3  );
+            Console.WriteLine("r4 : "+r4  );
+        }
+
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+
+        }
+        public static int Plus(int a,int b,int c)
+        {
+            return a + b + c;
+        }
+        public static int Plus(double a,double b)
+        {
+            return a + b;
+        }
+        public static int Plus(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Ch04/5_MethodParameter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 : 2022/06/07
+ * 이름 : 이서희
+ * 내용 : 메서드 출력전용 매개변수
+ * 
+ */
+namespace Ch04
+{
+    internal class MethodParameter
+    {
+        static void Main(string[] args)
+        {
+            //ref 매개변수
+            int num1 = 10;
+            int num2 = 3;
+            int num3 = 0;
+            int num4 = 0;
+
+            Divider1(num1, num2, ref num3, ref num4);
+
+            Console.WriteLine("몫 : {0}, 나머지 : {1}", num3, num4);
+
+            //out 매개변수
+        }
+
+        public static void Divider1(int a, int b, ref int quotient, ref int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+
+        }
+
+        public static void Divider2(int a, int b, ref int quotient, ref int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+
+        }
+    }
+    
+
+}
