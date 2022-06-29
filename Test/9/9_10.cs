bdb9db37c99865244f9ef7e9242489f11a66b659
Your Name <sshh7050@naver.com>
2022-06-29 17:06:40 +0900

    Final test3

--- Test/9/9_10.cs
+++ Test/9/9_10.cs
@@ -8,5 +8,24 @@ namespace Test._9
 {
     internal class _9_10
     {
+        static void Main1(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            double[] d = { 0.1, 0.2, 0.3 };
+            string[] s = { "apple", "banana", "cherry" };
+
+            PrintArray<int>(a);
+            PrintArray<double>(d);
+            PrintArray<string>(s);
+        }
+
+        public static void PrintArray<T>(T[]arr)
+        {
+            foreach(var item in arr)
+            {
+                Console.Write("{0,8}", item);
+            }
+            Console.WriteLine();
+        }
     }
 }
